ac915537c34a6c428b9304d7d2bb85553f9f5af2
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-07-23 19:17:29 -0300

    fix site url 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://brunomollo.github.io/portfolio",
+  site: "https://brunomollo.github.io",
   base: "porfolio",
 });
